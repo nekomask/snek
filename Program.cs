410e6cf0aee698bae24282f312f005d749b00c4f
kittenmasque <volaju@gmail.com>
2023-03-28 20:34:59 -0700

    Instantiated Game class with game border

--- Program.cs
+++ Program.cs
@@ -4,17 +4,27 @@ public class Game
 {
     public int Width { get; set; }
     public int Height { get; set; }
-}
 
-// Drawing the game border on the console
-
-public void DrawBorder()
-{
-    for (int i = 0; i < Height; i++)
+    public Game(int width, int height)
     {
-        for (int j = object; j < Width; j++)
+        Width = width;
+        Height = height;
+    }
+
+    // Drawing the game border on the console
+
+    public void DrawBorder()
+{
+        for (int i = 0; i < Height; i++)
         {
-            if (i == 0 || i == Height - 1 || j == 0 || j == Width - 1 )
-                Console.Write(#);
+            for (int j = object; j < Width; j++)
+            {
+                if (i == 0 || i == Height - 1 || j == 0 || j == Width - 1)
+                    Console.Write(#);
+                    else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
         }
+    }
 }
